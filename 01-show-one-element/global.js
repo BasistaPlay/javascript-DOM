8c1c3b2cdc92d4de3412a631c5caa4d12a93ff3e
Edvards.T.M <BasistaPlay@gmail.com>
2021-03-22 11:23:21 +0200

    1uzd

--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -1,17 +1,21 @@
-window.addEventListener("load", function(){
-
-  // Here is some pseudo-code to help you get started:
-
-  // 1. Get the DOM element which will be clicked.
-
-  // 2. Add a listener for the 'click' event onto that element.
-
-  // 3. The block for the listener should get the DOM
-  //    element containing the text to reveal.
-
-  // 4. Modify that DOM element's style to change it's 'display'
-  //    from a hidden value to a shown value.
-
-  // 5. Also modify the DOM to hide the "More info..." link.
   
+window.addEventListener("load", function(){
+  const button = document.querySelector("#more_text_link")
+  const textToShow = document.querySelector("#more_text_content")
+  textToShow.style.display = "none"
+
+
+  button.addEventListener('click', function(e) {
+      textToShow.style.display = "initial"
+      button.style.display = "none"
+  })
 });
+
+
+/*
+
+1.Kad parlādē lapu tad vis atgriezas, jo visi skriti arī restartejas.
+2.Skripts nestrādās, jo html klases neredzēs js.
+3.
+
+*/
