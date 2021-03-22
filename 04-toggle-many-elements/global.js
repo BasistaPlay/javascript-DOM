7deaf3b70af256163c4a54a1c2126dedc3241d82
Edvards.T.M <BasistaPlay@gmail.com>
2021-03-22 23:09:05 +0200

    4uzd

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,26 @@
-// Your JavaScript goes here.
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const button = document.querySelector('#button')
+    const elements = document.querySelectorAll('.toggle_me')
+
+
+    var toggleStatus = false
+    button.addEventListener('click', function(e) {
+        if (toggleStatus == false)
+        {
+            elements.forEach(function(element) {
+                toggleStatus = true
+                element.style.display = 'initial'
+            })
+        }
+        else
+        {
+            elements.forEach(function (element) {
+                toggleStatus = false
+                element.style.display = 'none'
+            })
+        }
+    })
+
+})
